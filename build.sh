d861ab8876e1486f3eead06c1374f6d927d4f4eb
AdderlyMendoza <aderly19xd@ejemplo.com>
2025-03-26 11:16:54 -0500

    add mejoras in static

--- build.sh
+++ build.sh
@@ -1,11 +1,31 @@
+# #!/usr/bin/env bash
+# # Exit on error
+# set -o errexit
+
+# # pip install -r requirements.txt
+
+# # Convert static asset files
+# python manage.py collectstatic --no-input
+
+# # Apply any outstanding database migrations
+# python manage.py migrate
+
+
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# pip install -r requirements.txt
+# 1. Instalar dependencias
+pip install -r requirements.txt
 
-# Convert static asset files
-python manage.py collectstatic --no-input
+# 2. Crear directorios necesarios
+mkdir -p staticfiles
+mkdir -p media
 
-# Apply any outstanding database migrations
+# 3. Colectar archivos estáticos (con limpieza)
+python manage.py collectstatic --noinput --clear
+
+# 4. Aplicar migraciones
 python manage.py migrate
+
+# 5. Opcional: Comprimir estáticos (para WhiteNoise)
+python manage.py compress --force
--- congreso_fismat/settings.py
+++ congreso_fismat/settings.py
@@ -151,19 +151,18 @@ STATIC_URL = "static/"
 #     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
-    # Configuración para media files en producción (usar S3 es mejor)
+if DEBUG:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    # Configuración para producción (S3)
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-else:
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
     
     
 
