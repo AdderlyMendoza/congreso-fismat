a988b2e7b2e49e8317b4d91b9debabbf8cf555be
AdderlyMendoza <aderly19xd@ejemplo.com>
2025-04-08 12:32:07 -0500

    add lugares turisticos y hotel sonesta

--- congreso_fismat/settings.py
+++ congreso_fismat/settings.py
@@ -22,7 +22,7 @@ import dj_database_url # - P
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production  
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
