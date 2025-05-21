2fc2213d4724b456b1784e5798f389fec48070c9
AdderlyMendoza <aderly19xd@gmail.com>
2025-05-21 14:18:45 -0500

    fixed BCP and CCI, tam max de docs 5mbs

--- registro/urls.py
+++ registro/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views  # Importa las vistas de la app 'registro'
 
 urlpatterns = [
+    
     path('', views.registro_view, name='registro_form'),  # Ruta del formulario de registro
     path('validar-duplicados/', views.validar_duplicados, name='validar_duplicados'),  # Nueva ruta para validar duplicados
     path('registro-constancia/<str:dni>/', views.registro_constancia, name='registro_constancia'),  # Ruta para generar la constancia de registro
