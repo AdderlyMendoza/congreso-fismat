a044691d9d7618d532aaa2ece8380a774cc73112
AdderlyMendoza <aderly19xd@gmail.com>
2025-06-27 12:53:54 -0500

    add pag bases cientificas

--- web/urls.py
+++ web/urls.py
@@ -3,4 +3,5 @@ from . import views  # Importa las vistas de la app 'web'
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),  # Ruta de la landing page
+    path('bases-cientificas', views.bases_cientificas, name='bases-cientificas'),  # Ruta de la landing page
 ]
--- web/views.py
+++ web/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'web/pagWeb.html')
+
+
+def bases_cientificas(request):
+    return render(request, 'web/bases.html')
